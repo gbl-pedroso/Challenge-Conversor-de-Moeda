267d6fbf8870a5015e9c40e9a11171af69e7e937
Gabriel <gpedroso1995@gmail.com>
2025-05-07 13:58:54 -0300

    removido construtor vazio MoedaDTO

--- src/dto/MoedaDTO.java
+++ src/dto/MoedaDTO.java
@@ -1,7 +1,4 @@
 package dto;
 
-public record MoedaDTO(String base_code, String target_code, Double conversion_result ) {
-
-    public record MoedaDTOVazio(){}
-}
+public record MoedaDTO(String base_code, String target_code, Double conversion_result ) {}
 
